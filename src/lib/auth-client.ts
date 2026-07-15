9d5dad5f2562c3acd633913d80c75c8e2fc1ac1c
juadul <parallaxhub@gmail.com>
2026-07-15 14:21:56 +0800

    auth file fixed

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,10 @@
-import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.BETTER_AUTH_URL
-})
+import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import type { auth } from "@/lib/auth"; 
 
-export const { signIn, signUp, signOut,  useSession } = createAuthClient()
+export const authClient = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+  plugins: [inferAdditionalFields<typeof auth>()],
+});
+
+export const { signIn, signUp, signOut, useSession } = authClient;
